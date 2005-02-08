27cb3846f92d6a6dc43305d28db9deb54a43a40b
Alistair Riddoch <alriddoch@googlemail.com>
2005-02-08 22:21:52 +0000

    *** empty log message ***

--- src/calamari.c
+++ src/calamari.c
@@ -208,6 +208,7 @@ void draw_grid()
     // Store this position
     glPushMatrix();
 
+    // Draw vertical lines
     glVertexPointer(3, GL_FLOAT, 0, vertical_line_vertices);
     for (int i = 0; i <= grid_width; ++i) {
         glDrawArrays(GL_LINES, 0, 2);
@@ -218,6 +219,7 @@ void draw_grid()
     glPopMatrix();
     glPushMatrix();
 
+    // Draw horizontal lines
     glVertexPointer(3, GL_FLOAT, 0, horizontal_line_vertices);
     for (int j = 0; j <= grid_height; ++j) {
         glDrawArrays(GL_LINES, 0, 2);
@@ -228,6 +230,7 @@ void draw_grid()
     glPopMatrix();
     glPushMatrix();
 
+    // Draw blocks whereever one should be placed on the grid.
     for(int i = 0; i < grid_width; ++i) {
         for(int j = 0; j < grid_height; ++j) {
             if ((slots[i][j])) {
@@ -240,6 +243,7 @@ void draw_grid()
 
     glColor3f(1.0, 1.0, 1.0);
 
+    // Draw the user controlled block
     glPopMatrix();
     glTranslatef(block_x, block_y, 0.f);
     draw_unit_cube();
@@ -298,7 +302,8 @@ void render_interface()
     // Disable the depth test, as its not useful when rendering text
     glDisable(GL_DEPTH_TEST);
 
-    //
+    // Print the number of frames per second. This is essential performance
+    // information when developing 3D graphics.
     glTranslatef(5.f, 5.f, 0);
     glColor3f(1.f, 1.f, 1.f);
     sprintf(buf, "FPS: %d", average_frames_per_second);
@@ -307,15 +312,23 @@ void render_interface()
 
 // Handle a mouse click. Coordinates are given in OpenGL style coordinates
 // with the origin in the bottom left.
+// This function exactly mimics rendering a scene, and then detects which
+// elements of the scene were under the mouse pointer when the click occured.
 void mouse_click(unsigned int x, unsigned int y)
 {
     GLuint selectBuf[512];
     GLfloat square_vertices[] = { 0.f, 0.f, 0.f, 1.f, 0.f, 0.f,
                                   1.f, 1.f, 0.f, 0.f, 1.f, 0.f };
 
+    // Put OpenGL into Selection mode rather than Render mode. This disables
+    // rendering to the window.
     glSelectBuffer(512,selectBuf);
     glRenderMode(GL_SELECT);
 
+    
+    // This is just like setting up the projection for normal rendering
+    // except we use gluPickMatrix to specify the portion of the screen
+    // we are interested in - the 1 pixel square under the mouse pointer.
     {
         // Set the projection transform
         glMatrixMode(GL_PROJECTION);
@@ -329,14 +342,29 @@ void mouse_click(unsigned int x, unsigned int y)
         camera_pos();
     }
 
+    // Each thing we render will have a numerical name that we must later use
+    // to identify which thing we rendered was under the mouse pointer.
+    // The names are pushed onto a stack, so a thing can have more than
+    // one. We will just have one name per thing, so we start by pushing a
+    // token name onto the stack.
     glInitNames();
+    glPushName(0);
+
+    // Here we will draw the grid exactly as it was drawn when rendering to
+    // the screen, except we draw a quad on each square rather than just
+    // drawing lines. If you want to detect clicking on other things, you
+    // will need to modify the code here.
     glTranslatef(-(float)grid_width/2.0f, -(float)grid_height/2.0f, 0.0f);
 
     glVertexPointer(3, GL_FLOAT, 0, square_vertices);
-    glPushName(0);
 
     for(int i = 0; i < grid_width; ++i) {
         for(int j = 0; j < grid_height; ++j) {
+            // Before we render each grid square, we specify the name by
+            // loading it into the top of the name stack. The name
+            // is derived from the coordinates of the square, so that later
+            // when we get a name back, we can tell which square it corresponds
+            // to.
             glLoadName(i + j * grid_width);
             glDrawArrays(GL_QUADS, 0, 4);
             glTranslatef(0.0f, 1.0f, 0.0f);
@@ -344,22 +372,33 @@ void mouse_click(unsigned int x, unsigned int y)
         glTranslatef(1.0f, -grid_height, 0.0f);
     }
 
+    // We have now finished with the name stack, so we empty it.
     glPopName();
 
+    // Put OpenGL back into rendering mode. This has the side effect of
+    // returning how many hits we got in the selection code above.
     int hits = glRenderMode(GL_RENDER);
 
-    std::cout << "Got " << hits << " hits" << std::endl << std::flush;
+    // std::cout << "Got " << hits << " hits" << std::endl << std::flush;
 
+    // If we got no hits, the user clicked on empty space.
     if (hits == 0) {
         return;
     }
 
+    // This code searches through the results buffer to find what hits we
+    // got.
+
+    // ptr points to the beginning of the results
     GLuint * ptr = &selectBuf[0];
     GLuint minDepth = UINT_MAX, noNames = 0;
     GLuint * namePtr = 0;
     for (int i = 0; i < hits; i++) {
         int names = *(ptr++);
         std::cout << "{" << *ptr << "}";
+        // Check if this hit is closer to the viewer than the last one we
+        // processed. If this test passes, then this is the closest hit
+        // so far, so we record the necessary details.
         if (*ptr < minDepth) {
             noNames = names;
             minDepth = *ptr;
@@ -367,20 +406,29 @@ void mouse_click(unsigned int x, unsigned int y)
         }
         ptr += (names + 2);
     }
-    GLuint * nameItr = namePtr;
-    int closest = -1;
-    std::cout << "The closest hit has " << noNames << " names: " << std::endl << std::flush;
+    // Once this loop is complete, we have the details of the nearest hit
+    // stored, which is the thing we rendered that was closest to the viewer
+    // under the mouse pointer. This should be the thing the player was aiming
+    // to click on when they clicked.
 
+    // noNames = Number of names our hit has
+    // namePtr = Pointer to list of names
+
+    // Extract the X and Y coords of the clicked square from the hit name
     GLuint hitName = *namePtr;
     int hit_x = hitName % grid_width;
     int hit_y = hitName / grid_width;
 
-    std::cout << "We hit grid square " << hit_x << ", " << hit_y << std::endl << std::flush;
+    // Place or remove a block on the square the user clicked.
     if (hit_x < grid_width && hit_y < grid_height) {
         slots[hit_x][hit_y] = !slots[hit_x][hit_y];
     }
 }
 
+// This function is called every step_time milliseconds. In many games you
+// will want to use this function to update the game state. For example in
+// a Tetris game, this would be the place where the falling blocks were
+// moved down the grid.
 void step()
 {
 }
