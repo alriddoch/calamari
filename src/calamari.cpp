1bee550de2ad1492ad8b0fce886d4b88f4c9a00f
Al Riddoch <alriddoch@googlemail.com>
2015-01-05 01:02:51 +0000

    Clean up the shader code.

--- src/calamari.cpp
+++ src/calamari.cpp
@@ -240,6 +240,7 @@ SDL_Window * init_graphics()
         return nullptr;
     }
 
+  SDL_GL_SetSwapInterval(0);
 
   gProgramID = glCreateProgram();
 
@@ -257,10 +258,7 @@ SDL_Window * init_graphics()
     "  vec4 diffuse, ambient;"
     "  float NdotL;"
 
-    "  gl_Position = ftransform();"
-
     "  /* first transform the normal into eye space and normalize the result */"
-
     "  normal = normalize(gl_NormalMatrix * gl_Normal);"
 
     "  /* now normalize the light's direction. Note that according to the"
@@ -268,20 +266,18 @@ SDL_Window * init_graphics()
     "  we're talking about a directional light, the position field is actually"
     "  direction */"
     "  lightDir = normalize(vec3(gl_LightSource[1].position));"
-    "  /* compute the cos of the angle between the normal and lights direction."
 
+    "  /* compute the cos of the angle between the normal and lights direction."
     "  The light is directional so the direction is constant for every vertex."
     "  Since these two are normalized the cosine is the dot product. We also"
     "  need to clamp the result to the [0,1] range. */"
     "  NdotL = max(dot(normal, lightDir), 0.0);"
-    "  /* Compute the diffuse term */"
 
+    "  /* Compute the diffuse term */"
     "  diffuse = gl_FrontMaterial.diffuse * gl_LightSource[1].diffuse;"
     "  ambient = gl_FrontMaterial.ambient * gl_LightSource[1].ambient;"
     "  gl_FrontColor =  NdotL * diffuse + ambient;"
-    // "  normal = gl_NormalMatrix * gl_Normal;"
-    // "  vec4 vertex_in_modelview_space = gl_ModelViewMatrix * gl_Vertex;"
-    // "  vertex_to_light_vector = vec3(gl_LightSource[0].position * vertex_in_modelview_space);"
+    "  gl_Position = ftransform();"
     "}"
   };
 
