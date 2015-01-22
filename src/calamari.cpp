fc7df3d9c4ef598bebc5e5853161e5887fa93801
Alistair Riddoch <alriddoch@googlemail.com>
2015-01-22 17:03:02 +0000

    Fix comment formatting in the shader

--- src/calamari.cpp
+++ src/calamari.cpp
@@ -351,15 +351,15 @@ void main() {
   normal = normalize(gl_NormalMatrix * gl_Normal);
 
   /* now normalize the light's direction. Note that according to the
-  OpenGL specification, the light is stored in eye space. Also since
-  we're talking about a directional light, the position field is actually
-  direction */
+     OpenGL specification, the light is stored in eye space. Also since
+     we're talking about a directional light, the position field is actually
+     direction */
   lightDir = normalize(vec3(gl_LightSource[1].position));
 
   /* compute the cos of the angle between the normal and lights direction.
-  The light is directional so the direction is constant for every vertex.
-  Since these two are normalized the cosine is the dot product. We also
-  need to clamp the result to the [0,1] range. */
+     The light is directional so the direction is constant for every vertex.
+     Since these two are normalized the cosine is the dot product. We also
+     need to clamp the result to the [0,1] range. */
   NdotL = max(dot(normal, lightDir), 0.0);
 
   /* Compute the diffuse term */
